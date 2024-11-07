ab4bd9d19e0b692186f966e873033768a7b9f40f
asghar-0017 <asghar.usman0017@gmail.com>
2024-11-07 18:50:11 +0500

    .

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import goldLogo from '../../assets/images/DravantiME-Logo-Gold-MAIN-300x174.png';
+import '../../assets/style/divranti.css'
 
 const About = () => {
   return (
-    <div>
-      
+    <>
+    <div className='container'>
+      <h2 className='heading'>DRAVANTI Middle East</h2>
+      <img src={goldLogo} alt="Dravanti Logo" />
     </div>
-  )
-}
-
-export default About
+    <div className='description container'>Dravanti Middle East, known as “Dravanti”, “Dravanti Group”, or ‘DME’, is a commodity and real-world-asset (RWA) focused Virtual Asset Portfolio Provider.  Dravanti offers a highly secure, stable, and scalable Virtual Asset investment vehicle, offering investors and owners of the Token access to a diversified RWA portfolio.</div>
+    </>
+  );
+};
