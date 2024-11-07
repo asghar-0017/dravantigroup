82b2ce86de39f480a19217b6a6ae31b26d304959
Rehman810 <rehmankp186@gmail.com>
2024-11-07 18:27:34 +0500

    .

--- src/components/history/history.jsx
+++ src/components/history/history.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const History = () => {
   return (
-    <p style={{color: "#92BCB3", fontSize: "40px", fontWeight: "bold"}}>
+    <p className="container" style={{color: "#92BCB3", fontSize: "40px", fontWeight: "bold"}}>
       Over the next 18 months Dravanti Middle East will scale to deliver one of
       the largest Real-World-Asset on-chain Portfolios in the world. This will
       be executed through the stable and phased release of liquid commodities,
