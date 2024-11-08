fa99e88b2aad959e4ed57dfe5244712f19378b5a
asghar-0017 <asghar.usman0017@gmail.com>
2024-11-09 02:51:51 +0500

    .

--- src/pages/partner1/MBMEgroupMembers.jsx
+++ src/pages/partner1/MBMEgroupMembers.jsx
@@ -3,22 +3,42 @@ import { FaRegCheckCircle } from "react-icons/fa";
 import { Container, Typography, Box, Grid } from '@mui/material';
 import groupgraph from '../../assets/images/groupgraph.png'
 
+
+const data = [
+    {
+      title: "",
+      description: "High Transaction Volume: Successfully handling more than 90 million transactions, showcasing our robust and reliable systems."
+    },
+    {
+      title: "",
+      description: "Extensive User Base: Serving more than 3.2 million users, demonstrating our widespread adoption and trust among customers."
+    },
+    {
+      title: "",
+      description: "Significant Turnover: Achieving a remarkable turnover of more than 3 billion AED, reflecting our strong market presence and financial performance."
+    },
+    {
+      title: "",
+      description: "Diverse Client Portfolio: Catering to over 100 clients, including government, semi-government, and private sector entities, highlighting our versatility and broad reach."
+    }
+  ];
+
 const MBMEgroupMembers = () => {
   return (
     <Container>
-      <Box my={4} textAlign="center">
+      <Box my={4} textAlign="left">
         <Typography variant="body1" color="textSecondary">
           MBME Group has unparalleled innovation, seamless solutions, and a commitment to excellence, ensuring your business stays ahead in the competitive digital landscape.
         </Typography>
       </Box>
 
-      <Box my={4} textAlign="center">
+      <Box my={4} textAlign="left">
         <Typography variant="h4" component="h2">
           MBME Group by numbers
         </Typography>
       </Box>
 
-      <Box my={4} textAlign="center">
+      <Box my={4} textAlign="left">
         <Typography variant="body1" color="textSecondary">
           Experience the impressive achievements of MBME Group:
         </Typography>
@@ -26,48 +46,7 @@ const MBMEgroupMembers = () => {
 
       <Box my={4}>
 
-        {/* <Grid container spacing={4} mt={4} justifyContent="center">
-          <Grid item xs={12} sm={6} md={3}>
-            <Box textAlign="center">
-              <FaRegCheckCircle size={40} color="#3f51b5" />
-              <Typography variant="h6" mt={2}>MBME Pay</Typography>
-              <Typography variant="body2" color="textSecondary">
-                A versatile payment services provider revolutionizing the industry with product aggregation services, payment channels, and gateways.
-              </Typography>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Box textAlign="center">
-              <FaRegCheckCircle size={40} color="#3f51b5" />
-              <Typography variant="h6" mt={2}>MBME Neo</Typography>
-              <Typography variant="body2" color="textSecondary">
-                A groundbreaking digital banking initiative on a mission to establish new financial institutions and redefine the banking experience.
-              </Typography>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Box textAlign="center">
-              <FaRegCheckCircle size={40} color="#3f51b5" />
-              <Typography variant="h6" mt={2}>MBME Services</Typography>
-              <Typography variant="body2" color="textSecondary">
-                At the forefront of innovation, MBME Wow Pay leads in proprietary financial technology, platforms, and government digital platforms.
-              </Typography>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <Box textAlign="center">
-              <FaRegCheckCircle size={40} color="#3f51b5" />
-              <Typography variant="h6" mt={2}>MBME Ventures</Typography>
-              <Typography variant="body2" color="textSecondary">
-                Focused on strategic investments and joint ventures to drive growth and innovation.
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid> */}
-           <div className="MBMEgroup-ecosystem">
+           {/* <div className="MBMEgroup-ecosystem">
                 <div className="MBMEgroup-icons">
                     <div className="MBMEgroup-icon-box">
                         <span className="text-content">
@@ -98,7 +77,21 @@ const MBMEgroupMembers = () => {
                         </div>
                     </div>
                 </div>
+            </div> */}
+
+<div className="MBMEgroup-icons">
+          {data.map((item, index) => (
+            <div className="MBMEgroup-icon-box" key={index}>
+              <span className="icon-title">
+                <span>
+                  <FaRegCheckCircle className="icon" />
+                </span>
+                <p className="title">{item.title}</p>
+              </span>
+              <p className="description">{item.description}</p>
             </div>
+          ))}
+        </div>
       </Box>
 
       
