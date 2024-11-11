48e1196c2c887be7550538cfc0f35f3447064522
muneeb-babar <muneeb63ahmad63@gmail.com>
2024-11-11 17:51:38 +0500

    .

--- src/pages/Terms/Terms.jsx
+++ src/pages/Terms/Terms.jsx
@@ -33,7 +33,7 @@ const Terms = () => {
 </ul>
 
 
-      <h4><strong>Definitions</strong></h4>
+      <h4><strong>1. Definitions</strong></h4>
 <p>The following terminology applies to these Terms and Conditions, Privacy Statement, and Disclaimer Notice and all Agreements:</p>
 <ul>
   <li>“Client”, “You”, and “Your” refers to you, the person who logs on to this website and is compliant with the Company’s terms and conditions.</li>
@@ -42,18 +42,18 @@ const Terms = () => {
 </ul>
 
 
-      <h4><strong>Use of Our Service</strong></h4>
+      <h4><strong>2. Use of Our Service</strong></h4>
       <p>By accessing or using our Service, you agree to comply with and be bound by these Terms and Conditions and our Privacy Policy. You must not use our Service for any unlawful or prohibited purpose.</p>
 
-      <h4><strong>Eligibility</strong></h4>
+      <h4><strong>3. Eligibility</strong></h4>
       <p>To use our Service, you must be at least 18 years old or the legal age of majority in your jurisdiction. By using our Service, you represent and warrant that you meet these eligibility requirements.</p>
 
-      <h4><strong>Accounts</strong></h4>
+      <h4><strong>4. Accounts</strong></h4>
       <p>When you create an account with us, you must provide accurate, complete, and current information at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</p>
       <p>You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.</p>
       <p>You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
 
-      <h4><strong>Intellectual Property</strong></h4>
+      <h4><strong>5. Intellectual Property</strong></h4>
       <p>Unless otherwise stated, Dravanti Middle East and/or its licensors own the intellectual property rights for all material on this website. All intellectual property rights are reserved. You may access this from Dravanti Middle East for your own personal use, subject to restrictions set in these terms and conditions.</p>
       <p>You must not:</p>
       <ul>
@@ -63,7 +63,7 @@ const Terms = () => {
         <li>Redistribute content from Dravanti Middle East</li>
       </ul>
 
-      <h4><strong>Prohibited Activities</strong></h4>
+      <h4><strong>6. Prohibited Activities</strong></h4>
       <p>You may not access or use the website for any purpose other than that for which we make the website and our services available. The website may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us.</p>
       <p>Prohibited activities include, but are not limited to:</p>
       <ul>
@@ -81,7 +81,7 @@ const Terms = () => {
         <li>Using the website in a manner inconsistent with any applicable laws or regulations.</li>
       </ul>
 
-      <h4><strong>Limitation of Liability</strong></h4>
+      <h4><strong>7. Limitation of Liability</strong></h4>
       <p>To the fullest extent permitted by applicable law, in no event will Dravanti Middle East, its affiliates, directors, employees, or agents be liable to you for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses, resulting from:</p>
       <ul>
         <li>Your use of or inability to use the Service;</li>
@@ -91,7 +91,7 @@ const Terms = () => {
         <li>Any errors or omissions in any content or for any loss or damage incurred as a result of your use of any content posted, emailed, transmitted, or otherwise made available through the Service.</li>
       </ul>
 
-      <h4><strong>Indemnification</strong></h4>
+      <h4><strong>8. Indemnification</strong></h4>
       <p>You agree to defend, indemnify, and hold harmless Dravanti Middle East and its subsidiaries, agents, licensors, managers, and other affiliated companies, and their employees, contractors, agents, officers, and directors, from and against any and all claims, damages, obligations, losses, liabilities, costs, or debt, and expenses (including but not limited to attorney’s fees) arising from:</p>
       <ul>
         <li>Your use of and access to the Service, including any data or content transmitted or received by you;</li>
@@ -101,20 +101,20 @@ const Terms = () => {
         <li>Any claim or damages that arise as a result of any of your User Content or any that is submitted via your account.</li>
       </ul>
 
-      <h4><strong>Termination</strong></h4>
+      <h4><strong>9. Termination</strong></h4>
       <p>We may terminate or suspend your account and bar access to the Service immediately, without prior notice or liability, under our sole discretion, for any reason whatsoever and without limitation, including but not limited to a breach of the Terms.</p>
       <p>If you wish to terminate your account, you may simply discontinue using the Service.</p>
       <p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.</p>
 
-      <h4><strong>Governing Law</strong></h4>
+      <h4><strong>10. Governing Law</strong></h4>
       <p>These Terms shall be governed and construed in accordance with the laws of the United Arab Emirates, without regard to its conflict of law provisions.</p>
       <p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service and supersede and replace any prior agreements we might have had between us regarding the Service.</p>
 
-      <h4><strong>Changes to Terms and Conditions</strong></h4>
+      <h4><strong>11. Changes to Terms and Conditions</strong></h4>
       <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. What constitutes a material change will be determined at our sole discretion.</p>
       <p>By continuing to access or use our Service after any revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
 
-      <h4><strong>Contact Us</strong></h4>
+      <h4><strong>12. Contact Us</strong></h4>
       <p>If you have any questions about these Terms, please contact us:</p>
       <ul>
       <li>Email: <a style={{ textDecoration: 'none', color: '#54595f' }} href="mailto:info@dravantigroup.com">info@dravantigroup.com</a></li>
